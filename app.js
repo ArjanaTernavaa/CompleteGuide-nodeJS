4f035c73e852abf28f828a1ec6cddcd7bf3e0bdf
ArjanaaTernava <arjana.ternava@evonem.com>
2023-09-06 10:19:31 +0200

    Creating routes using vanilla js and node

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const routes = require("./routes");	
 
-const server = http.createServer(routes); //execute the function stored in routes
+const server = http.createServer(routes.handler); //execute the function stored in routes
 
 server.listen(3000);
--- routes.js
+++ routes.js
@@ -37,4 +37,6 @@ const requestHandler = (req,res) => {
 	res.end();
 }
 
-module.exports = requestHandler;
+module.exports = {
+    handler: requestHandler //multiple exports
+};
