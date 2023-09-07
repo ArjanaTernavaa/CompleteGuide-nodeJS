eb8606f40f7eb24ce128e87c7c97089627e476ee
ArjanaaTernava <arjana.ternava@evonem.com>
2023-09-07 10:38:47 +0200

    Using ejs as an engine template

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require('express-handlebars');
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -10,8 +9,8 @@ const shopRoutes = require("./routes/shop");
 
 const app = express(); //alot of logic is in the app, it handles routes dmth mujna me bo pass the create server 
 
-app.engine('.hbs', expressHbs.engine({layoutsDir: 'views/layouts/', defaultLayout: 'main', extname: 'hbs'})); //register a engine that is not registered like handlebars
-app.set('view engine', 'hbs'); //match the name with the line above
+// app.engine('.hbs', expressHbs.engine({layoutsDir: 'views/layouts/', defaultLayout: 'main', extname: 'hbs'})); //register a engine that is not registered like handlebars
+app.set('view engine', 'ejs'); //match the name with the line above
 app.set('views','views'); //by default views jane located n views
 
 app.use(bodyParser.urlencoded({ extended: false })); //me parse the body qe vjen nga ni request
@@ -21,7 +20,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).render('404',{docTitle:"Page not found"});
+    res.status(404).render('404',{docTitle:"Page not found", path:''});
 })
 
 app.listen(3000);
